84a8e55e442a02f048d5c9b05fc83f7de1e4f44f
J. Zebedee <zebedee@code.gripe>
2024-12-10 14:48:24 -0600

    Add review MI to stack

--- src/base/Program.cs
+++ src/base/Program.cs
@@ -10,7 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-return await Pulumi.Deployment.RunAsync(async () =>
+return await Pulumi.Deployment.RunAsync(() =>
 {
     var az = new Pulumi.Config("azure-native");
 
@@ -22,16 +22,25 @@ return await Pulumi.Deployment.RunAsync(async () =>
     var base_org = config.Require("base_org");
     var base_repo = config.Require("base_repo");
 
-    // base
-    var base_mi = await BuildBaseManagedIdentity(subscriptionId, location, oidcOrg: base_org, oidcRepo: base_repo);
+    Dictionary<string, object?> outputs = [];
 
-    return new Dictionary<string, object?>
+    // base
+    if (Pulumi.Deployment.Instance is { StackName: "base" })
     {
-        ["mi-shared-review:principalId"] = base_mi.PrincipalId
-    };
+        var base_mi = BuildBaseManagedIdentity(subscriptionId, location, oidcOrg: base_org, oidcRepo: base_repo);
+        outputs.Add("mi-shared-review:principalId", base_mi.PrincipalId);
+    }
+    else if (config.Require("pr_number") is string pr)
+    {
+        var uniqueId = $"{base_org}_{base_repo}_pr{pr}";
+        var review_mi = BuildReviewManagedIdentity(subscriptionId, location, oidcOrg: base_org, oidcRepo: base_repo, uniqueId);
+        outputs.Add("mi-review-app", review_mi.Name);
+    }
+
+    return outputs;
 });
 
-static async Task<UserAssignedIdentity> BuildBaseManagedIdentity(string subscriptionId, string location, string oidcOrg, string oidcRepo)
+static UserAssignedIdentity BuildBaseManagedIdentity(string subscriptionId, string location, string oidcOrg, string oidcRepo)
 {
     string rgName = "rg-shared-review";
     string identityName = "mi-shared-review";
@@ -64,6 +73,45 @@ static async Task<UserAssignedIdentity> BuildBaseManagedIdentity(string subscrip
         PrincipalId = managedIdentity.PrincipalId,
         PrincipalType = PrincipalType.ServicePrincipal,
         Scope = $"/subscriptions/{subscriptionId}",
+        RoleDefinitionId = ra_roleDefId,
+    });
+
+    return managedIdentity;
+}
+
+static UserAssignedIdentity BuildReviewManagedIdentity(string subscriptionId, string location, string oidcOrg, string oidcRepo, string uniqueId)
+{
+    string rgName = $"rg-review-{uniqueId}";
+    string identityName = $"mi-review-{uniqueId}";
+
+    var managedIdentity = new UserAssignedIdentity(identityName, new()
+    {
+        Location = location,
+        ResourceGroupName = rgName,
+        ResourceName = identityName,
+    });
+
+    string fedCred_name = $"gh-pr_{oidcOrg}_{oidcRepo}";
+    var federatedIdentityCredential = new FederatedIdentityCredential(fedCred_name, new()
+    {
+        Audiences = ["api://AzureADTokenExchange"],
+        FederatedIdentityCredentialResourceName = fedCred_name,
+        Issuer = "https://token.actions.githubusercontent.com",
+        ResourceGroupName = rgName,
+        ResourceName = identityName,
+        Subject = $"repo:{oidcOrg}/{oidcRepo}:pull_request",
+    });
+
+    const string roleDefinitionId_owner = "8e3af657-a8ff-443c-a75c-2fe8c4bcb635";
+
+    string ra_name = $"ra-owner_{identityName}";
+    string ra_roleDefId = $"/subscriptions/{subscriptionId}/providers/Microsoft.Authorization/roleDefinitions/{roleDefinitionId_owner}";
+
+    var roleAssignment = new RoleAssignment(ra_name, new()
+    {
+        PrincipalId = managedIdentity.PrincipalId,
+        PrincipalType = PrincipalType.ServicePrincipal,
+        Scope = $"/subscriptions/{subscriptionId}/resourceGroups/{rgName}",
         RoleDefinitionId = ra_roleDefId,
     });
 
